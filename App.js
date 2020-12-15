3cbe442b9a157c1ee6f9352b709d3073e98c2be8
leonardo <leonardomagerote@gmail.com>
2020-12-15 13:18:28 -0300

    add

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Linha from './components/Linha';
+import {Header} from 'react-native-elements';
+import api from './api';
 
-export default function App() {
-  return (
+const App = () => {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    carregaFilmes();
+  }, []);
+
+  async function carregaFilmes(){
+    setFilmes([]);
+    const response = await api.get('/db');
+    setFilmes(response['data'].dados);
+}
+
+  return( 
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <Header
+          backgroundColor= '#000'
+          centerComponent={{ icon: 'menu', color: '#fff' }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <FlatList
+        data={filmes}
+        keyExtractor={(item)=> item.key.toString()}
+        renderItem={ ({item}) => 
+          <TouchableOpacity>
+            <Linha nome={item.nome} foto={item.img} profissao={item.profissao} telefone={item.telefone} avaliacao={item.avaliacao} />
+          </TouchableOpacity>
+      }
+      />
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontFamily: 'arial',
   },
 });
+
+
+export default App;
