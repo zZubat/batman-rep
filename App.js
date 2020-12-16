01e17cdfe42da35b9b281bc5352e292c8af50688
léo Miranda <leonardomagerote@gmail.com>
2020-12-15 21:22:10 -0300

    add

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Linha from './componentes/Linha';
 import {Header} from 'react-native-elements';
 import api from './data/Api';
